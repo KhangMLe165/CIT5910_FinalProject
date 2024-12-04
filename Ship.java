512cf54632d0c2fb8d977c65cfb9745c27ea1c44
khangmle <khang16le@gmail.com>
2024-12-04 16:14:27 -0500

    Commented Getters & Setters

--- Ship.java
+++ Ship.java
@@ -16,27 +16,36 @@ public abstract class Ship {
     }
 
     // Getters and setters
+    // Allow controlled access and modification of the ship's properties
     public int getLength() {
         return length;
     }
 
+    // Retrieves the row index of the ship's bow.
     public int getBowRow() {
         return bowRow;
     }
 
+    // Sets the row index of the ship's bow.
     public void setBowRow(int bowRow) {
         this.bowRow = bowRow;
     }
 
+    //Retrieves the column index of the ship's bow.
     public int getBowColumn() {
         return bowColumn;
     }
 
+    //Sets the column index of the ship's bow.
     public void setBowColumn(int bowColumn) {
         this.bowColumn = bowColumn;
     }
 
+    //Returns whether the ship is placed horizontally.
     public boolean isHorizontal() {
         return horizontal;
     }
 
+    //Sets the orientation of the ship.
+    public void setHorizontal(boolean horizontal) {
+        this.horizontal = horizontal;
