27ed047bf5f60b0f7046000c700e26a5f2eb2fe9
khangmle <khang16le@gmail.com>
2024-12-04 16:08:41 -0500

    Revamping comments

--- Ship.java
+++ Ship.java
@@ -1,10 +1,10 @@
 public abstract class Ship {
 
-    protected int bowRow;        // The row of the bow (front) of the ship
-    protected int bowColumn;     // The column of the bow (front) of the ship
+    protected int bowRow;        // Stores the row index of the ship's bow (front) on the grid.
+    protected int bowColumn;     // Stores the column index of the ship's bow.
     protected int length;        // The number of tiles occupied by the ship
-    protected boolean horizontal; // True if the ship is horizontal, false otherwise
-    protected boolean[] hit;     // Array tracking hits on the ship
+    protected boolean horizontal; // True if the ship is horizontal, false if vertical
+    protected boolean[] hit;     // : An array representing each segment of the ship. true at an index means that part of the ship has been hit
 
     /**
      * Constructor for Ship.
@@ -12,5 +12,7 @@ public abstract class Ship {
      */
     public Ship(int length) {
         this.length = length;
-        this.hit = new boolean[length];
+        this.hit = new boolean[length]; //Initializes the hit array with false values, indicating no parts of the ship have been hit.
     }
+
+
