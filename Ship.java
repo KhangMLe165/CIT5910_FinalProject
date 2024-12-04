6db54e795d6ec0e39108348d714964a85891c4ef
khangmle <khang16le@gmail.com>
2024-12-04 12:45:42 -0500

    Initilize Ship length

--- Ship.java
+++ Ship.java
@@ -1,2 +1,16 @@
-public class Ship {
-}
+public abstract class Ship {
+
+    protected int bowRow;        // The row of the bow (front) of the ship
+    protected int bowColumn;     // The column of the bow (front) of the ship
+    protected int length;        // The number of tiles occupied by the ship
+    protected boolean horizontal; // True if the ship is horizontal, false otherwise
+    protected boolean[] hit;     // Array tracking hits on the ship
+
+    /**
+     * Constructor for Ship.
+     * Initializes the hit array based on the length of the ship.
+     */
+    public Ship(int length) {
+        this.length = length;
+        this.hit = new boolean[length];
+    }
